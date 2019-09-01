69343e3d45dfa7aa999d4a1399b1b03d5fff9c04
Kanon Kakuno <yadex205@outlook.jp>
2019-09-01 19:52:02 +0700

    :sparkles: feat(a-textarrow): Implement.

---
+++ src/components/atoms/textarrow/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+interface NormalProps {
+  children: string;
+}
+
+interface LinkProps extends NormalProps {
+  href: string;
+  external?: boolean;
+}
+
+interface Component extends React.FC<NormalProps> {
+  Link: React.FC<LinkProps>;
+}
+
+const Textarrow: Component = props => (
+  <span className="a-textarrow">
+    <span className="a-textarrow__text">{props.children}</span>
+  </span>
+);
+
+Textarrow.Link = props =>
+  props.external ? (
+    <a href={props.href} rel="noreferrer noopener" className="a-textarrow a-textarrow--link">
+      <span className="a-textarrow__text">{props.children}</span>
+    </a>
+  ) : (
+    <Link to={props.href} className="a-textarrow a-textarrow--link">
+      <span className="a-textarrow__text">{props.children}</span>
+    </Link>
+  );
+
+Textarrow.Link.displayName = 'Textarrow.Link';
+
+export default Textarrow;
