65fb054b227584c8b89a4592a63fd575c3e27d62
Kanon Kakuno <yadex205@outlook.jp>
2019-09-07 22:51:16 +0700

    :+1: chore(transition): Go to top after transion.

--- src/components/organisms/transition/index.tsx
+++ src/components/organisms/transition/index.tsx
@@ -2,20 +2,31 @@ import React from 'react';
 import { Location } from '@reach/router';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
-const Transition: React.FC = props => (
-  <Location>
-    {context => (
-      <>
-        <TransitionGroup className="o-transition" appear={true}>
-          <CSSTransition classNames="o-transition__page-" timeout={500} key={context.location.pathname}>
-            <div className="o-transition__page">
-              {props.children}
-            </div>
-          </CSSTransition>
-        </TransitionGroup>
-      </>
-    )}
-  </Location>
-);
+const Transition: React.FC = props => {
+  function onEntered() {
+    window.scrollTo(0, 0);
+  }
+
+  return (
+    <Location>
+      {context => (
+        <>
+          <TransitionGroup className="o-transition" appear={true}>
+            <CSSTransition
+              classNames="o-transition__page-"
+              timeout={500}
+              onEntered={onEntered}
+              key={context.location.pathname}
+            >
+              <div className="o-transition__page">
+                {props.children}
+              </div>
+            </CSSTransition>
+          </TransitionGroup>
+        </>
+      )}
+    </Location>
+  );
+};
 
 export default Transition;
