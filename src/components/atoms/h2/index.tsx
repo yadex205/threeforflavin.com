77b7ac619675e32d27f47ac54fa94a76d176022f
Kanon Kakuno <yadex205@outlook.jp>
2019-09-07 17:06:24 +0700

    :green_heart: fix(eslint): Run npm run autofix.

--- src/components/atoms/h2/index.tsx
+++ src/components/atoms/h2/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-const H2: React.FC = props => (
-  <h2 className="a-h2">{props.children}</h2>
-);
+const H2: React.FC = props => <h2 className="a-h2">{props.children}</h2>;
 
 export default H2;
