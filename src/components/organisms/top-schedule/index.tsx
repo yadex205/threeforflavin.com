3ea686c414c78ae6e6351fe7e4d1d51a4aff1df6
Kanon Kakuno <yadex205@outlook.jp>
2019-09-07 16:03:27 +0700

    :+1: chore(pages/index): Use contentful source.

--- src/components/organisms/top-schedule/index.tsx
+++ src/components/organisms/top-schedule/index.tsx
@@ -1,29 +1,45 @@
 import React from 'react';
+import dayjs from 'dayjs';
 import Textarrow from 'components/atoms/textarrow';
 import TopPrimaryschedule from 'components/molecules/top-primaryschedule';
 import TopSubschedule from 'components/molecules/top-subschedule';
 
-const TopSchedule: React.FC = () => (
-  <div className="o-top-schedule">
-    <TopPrimaryschedule date="2019-11-23" title="Three for Flavin 3rd One-man Live!" venue="代官山 LOOP" href="#" />
-    <ul className="o-top-schedule__sublist">
-      <li>
-        <TopSubschedule date="2019-09-01" title={'ライブ名'.repeat(10)} venue="北参道 GRAPES" href="#" />
-      </li>
-      <li>
-        <TopSubschedule date="2019-09-01" title={'ライブ名'.repeat(1)} venue="北参道 GRAPES" href="#" />
-      </li>
-      <li>
-        <TopSubschedule date="2019-09-01" title={'ライブ名'.repeat(50)} venue="北参道 GRAPES" href="#" />
-      </li>
-      <li>
-        <TopSubschedule date="2019-09-01" title={'ライブ名'.repeat(7)} venue="北参道 GRAPES" href="#" />
-      </li>
-    </ul>
-    <div className="o-top-schedule__morewrapper">
-      <Textarrow.Link href="/live">MORE</Textarrow.Link>
+interface Props {
+  acts: {
+    date: string | Date | dayjs.Dayjs;
+    title: string;
+    venue: string;
+    href: string;
+  }[];
+}
+
+const TopSchedule: React.FC<Props> = props => {
+  const currentDate = new Date();
+  const acts = props.acts.filter(act => dayjs(act.date).isAfter(currentDate));
+
+  const nextAct = acts.shift();
+
+  return (
+    <div className="o-top-schedule">
+      {nextAct ? (
+        <TopPrimaryschedule date={nextAct.date} title={nextAct.title} venue={nextAct.venue} href={nextAct.href} />
+      ) : null}
+      {acts.length > 0 ? (
+        <>
+          <ul className="o-top-schedule__sublist">
+            {acts.map(act => (
+              <li key={act.href}>
+                <TopSubschedule date={act.date} title={act.title} venue={act.venue} href={act.href} />
+              </li>
+            ))}
+          </ul>
+          <div className="o-top-schedule__morewrapper">
+            <Textarrow.Link href="/live">MORE</Textarrow.Link>
+          </div>
+        </>
+      ) : null}
     </div>
-  </div>
-);
+  );
+};
 
 export default TopSchedule;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,55 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import { Live } from 'lib/models';
 import Layout from 'layout';
 import Section from 'components/organisms/section';
 import TopMainvisual from 'components/organisms/top-mainvisual';
 import TopSchedule from 'components/organisms/top-schedule';
 import TopSocial from 'components/organisms/top-social';
 
-export default function TopPage() {
+export const pageQuery = graphql`
+  query($builtat: Date!) {
+    allContentfulLive(
+      filter: { form: { eq: "threeforflavin" }, date: { gt: $builtat } }
+      sort: { fields: date, order: ASC }
+    ) {
+      edges {
+        node {
+          slug
+          title
+          date
+          venue
+        }
+      }
+    }
+  }
+`;
+
+interface Props {
+  data: {
+    allContentfulLive: {
+      edges: {
+        node: Pick<Live, 'slug' | 'title' | 'date' | 'venue'>;
+      }[];
+    };
+  };
+}
+
+export default function TopPage({ data: { allContentfulLive } }: Props) {
+  const acts = allContentfulLive.edges.map(({ node }) => ({
+    date: node.date,
+    title: node.title,
+    venue: node.venue,
+    href: `/live/${node.slug}`,
+  }));
+
   return (
     <Layout>
       <Section>
         <TopMainvisual />
       </Section>
       <Section>
-        <TopSchedule />
+        <TopSchedule acts={acts} />
       </Section>
       <Section>
         <TopSocial />
